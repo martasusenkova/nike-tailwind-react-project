eed969db4316f771fed17d01cb5bab09a3c9de09
martasusenkova <susenkova582@gmail.com>
2025-06-07 20:24:00 +0400

    feat: add review  and customer reviews sections

--- src/components/index.js
+++ src/components/index.js
@@ -3,8 +3,6 @@ import Nav from "./Nav";
 import ShoeCard from "./ShoeCard";
 import PopularProductCard from "./PopularProductCard";
 import ServiceCard from "./ServiceCard";
-// import ReviewCard from "./ReviewCard";
+import ReviewCard from "./ReviewCard";
 
-export { Button, Nav, ShoeCard, PopularProductCard, ServiceCard };
-
-// ReviewCard
+export { Button, Nav, ShoeCard, PopularProductCard, ServiceCard, ReviewCard };
