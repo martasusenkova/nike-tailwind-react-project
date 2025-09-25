96087f03d459d7f563534e869a1d8c52963cc378
martasusenkova <susenkova582@gmail.com>
2025-09-25 13:21:27 +0400

    fix images

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: react({
     base: "/nike-tailwind-react-project/",
-    jsxRuntime: "automatic",
+    plugins: [react()],
   }),
 });
